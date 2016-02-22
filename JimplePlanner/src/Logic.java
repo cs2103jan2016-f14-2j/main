e20e887ceb8eb0e97555525cca5399d1a422619f
Loh Victor <vic94loh@Hotmail.com>
2016-02-22 12:44:41 +0800

    Added Storage Component, partially implemented Event class
    Also updated .classpath file to path to javafx for my system, need to
    check if it works for other desktops

--- JimplePlanner/src/Logic.java
+++ JimplePlanner/src/Logic.java
@@ -1,3 +1,16 @@
+import java.util.Calendar;
+
+class Event {
+	private Calendar fromTime = null;
+	private Calendar ToTime = null;
+	private String title = null;
+	private String description = null;
+	
+	//Constructors
+	public Event(String aTitle){
+		title = aTitle;
+	}
+}
 
 public class Logic {
 
