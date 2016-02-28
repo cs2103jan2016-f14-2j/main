26c6c03a40a32c9d2914e2b7b57fa6e322af74f3
bsweeyee <Brandon@brandons-mbp-2>
2016-02-28 22:17:12 +0800

    created Edit Method to edit tasks

--- JimplePlanner/src/Logic.java
+++ JimplePlanner/src/Logic.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.TreeMap;
@@ -84,17 +85,22 @@ public class Logic {
 	private String EDIT_COMMAND_TWO_TIMINGS = "type \"edit\" + <your event> from <time> to <time>\n";
 	private String DISPLAY_COMMAND = "type \"display\"";
 	private String DELETE_COMMAND = "type \"delete\" <event name>";
-	
+
 	private String ADDED_FEEDBACK = "task added to planner\n";
 	private String EDITED_FEEDBACK = "task edited in planner\n";
+	
+	private String EDITED_FEEDBACK_ERROR = "task not found\n";
+	private String ADDED_FEEDBACK_ERROR = "could not add to task list\n";
 
 	private ArrayList<Event> temporaryHistory;
+	private ArrayList<Event> currentListOfTasksInFile;
 	Parser parser = new Parser();
 	Storage store = new Storage();
 
 	// Constructor
-	public Logic() {
+	public Logic() throws IOException {
 		temporaryHistory = new ArrayList<Event>();
+		currentListOfTasksInFile = store.getEvents();
 	}
 
 	public String execute(String inputString) throws IOException {
@@ -107,6 +113,7 @@ public class Logic {
 			feedback += addToTaskList(parsedInput.variableArray, inputString);
 			break;
 		case 6:
+			feedback += editTask(parsedInput.variableArray);
 			break;
 		}
 		return feedback;
@@ -136,14 +143,85 @@ public class Logic {
 			}
 		}
 		temporaryHistory.add(newTask);
-		store.isSaved(temporaryHistory);
-		return ADDED_FEEDBACK;
+		currentListOfTasksInFile.add(newTask);
+		if (store.isSaved(currentListOfTasksInFile))	{
+			return ADDED_FEEDBACK;
+		}
+		return ADDED_FEEDBACK_ERROR;
 	}
 	
-	public String editTask(String[] parsedInput) throws IOException	{
-		LinkedList<Event> allTasks = store.getEvents();
-		
-		return EDITED_FEEDBACK;
+	/**
+	 * edit a task
+	 * Condition: can only edit with line number
+	 */
+	
+	public String editTask(String[] parsedInput) throws IOException {
+		int taskNumber = Integer.parseInt(parsedInput[0]);
+		if (currentListOfTasksInFile.get(taskNumber) != null) {
+			for (int i = 0; i < parsedInput.length; i++) {
+				switch (i) {
+				case 1:
+					currentListOfTasksInFile.get(taskNumber).setTitle(parsedInput[i]);
+					break;
+				case 2:
+					currentListOfTasksInFile.get(taskNumber).setDescription(parsedInput[i]);
+					break;
+				case 3:
+					currentListOfTasksInFile.get(taskNumber).setFromDate(parsedInput[i]);
+					break;
+				case 4:
+					currentListOfTasksInFile.get(taskNumber).setToDate(parsedInput[i]);
+					break;
+				case 5:
+					currentListOfTasksInFile.get(taskNumber).setCategory(parsedInput[i]);
+					break;
+				}
+			}
+			if (store.isSaved(currentListOfTasksInFile))	{
+				return EDITED_FEEDBACK;
+			}
+		}
+		return EDITED_FEEDBACK_ERROR;
+	}
+	
+	/**
+	 * returns total number of word matches compared to an event
+	 * 
+	 */
+	private int matchingWordCount(String[] parsedInput, Event task) {
+		int count = 0;
+		for (int i = 0; i < parsedInput.length; i++) {
+			switch (i) {
+			case 1:
+				if (task.getTitle().contains(parsedInput[i])) {
+					count++;
+				}
+				break;
+			case 2:
+				if (task.getDescription().contains(parsedInput[i])) {
+					count++;
+				}
+				break;
+			case 3:
+				if (task.getFromTime().contains(parsedInput[i])) {
+					count++;
+				}
+				break;
+			case 4:
+				if (task.getToTime().contains(parsedInput[i])) {
+					count++;
+				}
+				break;
+			case 5:
+				if (task.getCategory().contains(parsedInput[i])) {
+					count++;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		return count;
 	}
 
 	public String helpCommand(String[] parsedInput) {
