1b3f5a5e25056040b0247f41d27277cc22179629
bsweeyee <Brandon@brandons-mbp-2>
2016-03-03 17:20:13 +0800

    added test for format time and format day

--- JimplePlanner/src/org/jimple/planner/LogicTest.java
+++ JimplePlanner/src/org/jimple/planner/LogicTest.java
@@ -17,14 +17,39 @@ public class LogicTest {
 	
 	@Test
 	public void EditShouldReturnFeedback() throws IOException	{
-		String[] parsedInput = {"1", "go school", "that means NUS", "2016-02-29T12:00", "", ""};
+		String[] parsedInput = {"1", "go school", "that means NUS", "2016-02-25T12:00", null, null};
 		assertEquals("task is edited", "task edited in planner\n", testLogic.editTask(parsedInput));
 	}
 	
 	@Test
-	public void ShouldReturnFeedbackFromExecute()	{
-		String input = "add something";
-		//assertEquals("returns feedback", "taskEd")
+	public void ShouldReturnCorrectMonth()	{
+		
 	}
+	
+	@Test
+	public void ShouldReturnCorrectDay()	{
+		assertEquals("correct day format", "05T", testLogic.checkDay("5"));
+		assertEquals("correct day format", "27T", testLogic.checkDay("27"));
+		assertEquals("correct day format", "03T", testLogic.checkDay("today"));
+		assertEquals("correct day format", "", testLogic.checkDay("may"));
+		assertEquals("correct day format", "", testLogic.checkDay("2016"));
+	}
+	
+	@Test
+	public void ShouldReturnCorrectTimeFormat()	{
+		assertEquals("right time format", "15:00", testLogic.checkTime("3pm"));
+		assertEquals("right time format", "03:30", testLogic.checkTime("3.30am"));
+	}
+	
+	@Test
+	public void ShouldReturnFormattedHoursMinutes()	{
+		assertEquals("returns correct hours and minutes", "15:30", testLogic.formatHoursMinutes("3.30", "pm"));
+		assertEquals("returns correct hours and minutes", "15:00", testLogic.formatHoursMinutes("3", "pm"));
+		assertEquals("returns correct hours and minutes", "00:00", testLogic.formatHoursMinutes("12", "am"));
+		assertEquals("returns correct hours and minutes", "11:30", testLogic.formatHoursMinutes("11.30", "am"));
+		assertEquals("returns correct hours and minutes", "09:05", testLogic.formatHoursMinutes("9.05", "am"));
+	}
+	
+	
 
 }
