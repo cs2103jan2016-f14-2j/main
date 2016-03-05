9707cf86c434203ca6f08f1d2634ef47ff734bd8
Walrus <walrus@hotmail.sg>
2016-03-05 13:55:24 +0800

    added keyEvent listeners for commandBox and tabPanels. arrow keys and escape keys added

--- JimplePlanner/src/org/jimple/planner/Controller.java
+++ JimplePlanner/src/org/jimple/planner/Controller.java
@@ -6,10 +6,19 @@ import java.util.ResourceBundle;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
@@ -25,6 +34,19 @@ public class Controller implements Initializable {
 
 	@FXML
 	Label messagePrompt;
+	
+	@FXML
+	AnchorPane mainController;
+	
+	@FXML
+	Tab agendaTab;
+	@FXML
+	Tab eventsTab;
+	@FXML
+	Tab todoTab;
+	
+	@FXML
+	TabPane tabPanes;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -37,16 +59,19 @@ public class Controller implements Initializable {
 	            commandBox.requestFocus();
 	        }
 	    });
+		
+		commandBoxListener();
+		tabPanesListener();
 	}
 	
-	public void enterButtonClicked() throws IOException {
+	public void enterTriggered() throws IOException {
 		String inputStr = getInputCommand();
 		if (!isEmpty(inputStr)) {
 			// System.out.println(inputStr);
 			String messageOutput = logic.execute(inputStr);
 			System.out.println(messageOutput);
 			messagePrompt.setText(messageOutput);
-
+			
 			FadeTransition ft = new FadeTransition(Duration.millis(3000), messagePrompt);
 			ft.setFromValue(1.0);
 			ft.setToValue(0.0);
@@ -55,6 +80,30 @@ public class Controller implements Initializable {
 
 			clearCommandBox();
 		}
+	}
+	
+	public void commandBoxListener(){		
+		commandBox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+		        @Override
+		        public void handle(KeyEvent t) {
+		            if(t.getCode() == KeyCode.ESCAPE)
+		            	tabPanes.requestFocus();
+		        }
+		    });		
+	}
+	
+	public void tabPanesListener(){		
+		tabPanes.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+		        @Override
+		        public void handle(KeyEvent t) {
+		        	if(!t.getCode().isArrowKey()){
+		        		commandBox.requestFocus();
+		        		commandBox.positionCaret(commandBox.getLength());
+		        	}
+		        }
+		    });		
 	}
 
 	public String getInputCommand() {
