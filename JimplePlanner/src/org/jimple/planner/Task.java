cba1b1df1af5897f9a421113871200046853534a
bsweeyee <Brandon@brandons-mbp-2>
2016-03-05 09:50:59 +0800

    separated Task class from Logic class

---
+++ JimplePlanner/src/org/jimple/planner/Task.java
@@ -0,0 +1,79 @@
+package org.jimple.planner;
+
+import java.time.LocalDateTime;
+
+class Task {
+	private LocalDateTime fromDateTime;
+	private LocalDateTime toDateTime;
+	private String title;
+	private String description;
+	private String category;
+
+	// Constructors
+	public Task(String aTitle) {
+		this.title = aTitle;
+		this.description = new String("");
+		this.category = new String("");
+		this.fromDateTime = null;
+		this.toDateTime = null;
+	}
+
+	public String getFromTime() {
+		if (fromDateTime == null) {
+			return "";
+		}
+		return fromDateTime.toString();
+	}
+
+	public void setFromDate(String dateTime) {
+		if (dateTime == null) {
+		} else {
+			this.fromDateTime = LocalDateTime.parse(dateTime);
+		}
+	}
+
+	public String getToTime() {
+		if (toDateTime == null) {
+			return "";
+		}
+		return toDateTime.toString();
+	}
+
+	public void setToDate(String dateTime) {
+		if (dateTime == null) {
+		} else {
+			this.toDateTime = LocalDateTime.parse(dateTime);
+		}
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		if (description == null) {
+			return "";
+		}
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		if (category == null) {
+			return "";
+		}
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
