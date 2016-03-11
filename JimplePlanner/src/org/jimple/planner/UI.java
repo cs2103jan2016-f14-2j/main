b69755e3243fb034b5f7bca951f58f82289b1718
Walrus <walrus@hotmail.sg>
2016-03-11 20:24:00 +0800

    Added application icon

--- JimplePlanner/src/org/jimple/planner/UI.java
+++ JimplePlanner/src/org/jimple/planner/UI.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class UI extends Application {
@@ -13,6 +14,7 @@ public class UI extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("JimpleUI.fxml"));
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add("application.css");
+			primaryStage.getIcons().add(new Image("Jimple-Icon.png"));
             primaryStage.setTitle("Jimple Planner");
 			primaryStage.setScene(scene);
 			primaryStage.show();
