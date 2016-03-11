ee9c1a596d050094b1e9b4894c6de88f3abbcec5
Walrus <walrus@hotmail.sg>
2016-03-11 17:15:05 +0800

    updated default planner.jim

---
+++ JimplePlanner/src/org/jimple/planner/ControllerTest.java
@@ -0,0 +1,19 @@
+package org.jimple.planner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+	
+	public void initialize(){
+		UI Jimple = new UI();
+		Jimple.begin();	
+	}
+
+	@Test
+	public void getTabName() {
+		fail("Not yet implemented");
+	}
+
+}
