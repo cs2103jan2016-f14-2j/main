f6f1a5bdd7ee003ad2128d76360398c6f2ee59c3
Luke <leeluke_9@live.com>
2016-03-14 19:10:26 +0800

    TimeFormat parsing is now completed for all current extended commands.

---
+++ JimplePlanner/src/org/jimple/planner/TimeParserTest.java
@@ -0,0 +1,24 @@
+package org.jimple.planner;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class TimeParserTest {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.print("Input an extended command: ");
+			String testExtendedCommand = sc.nextLine();
+			System.out.print("Input a time: ");
+			String testInput = sc.nextLine();
+			TimeParser tp = new TimeParser();
+			String t = tp.timeParser(testExtendedCommand, testInput);
+			if (t != null) {
+				System.out.println("Time parsed to:");
+				System.out.println(t);
+			}
+		}
+	}
+	
+}
