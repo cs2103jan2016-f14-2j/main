d0b33736d6cdcd5b9eb24f0a045f8df483ba39d9
bsweeyee <Brandon@r-99-100-25-172.comp.nus.edu.sg>
2016-03-21 17:29:43 +0800

    changed logic class as the subject

--- JimplePlanner/src/org/jimple/planner/observers/myObserver.java
+++ JimplePlanner/src/org/jimple/planner/observers/myObserver.java
@@ -1,6 +1,7 @@
 package org.jimple.planner.observers;
+import org.jimple.planner.logic.Logic;
 
 public abstract class myObserver {
-	protected Subject subject;
+	protected Logic logic;
 	public abstract void update();
 }
