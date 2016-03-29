0407455c6c88721c8fba28a7d2ef77a4407cf609
lohvht <vic94loh@hotmail.com>
2016-03-29 21:28:31 +0800

    created exceptions for labels, moved invalidtaskexceptions to exceptions package

--- JimplePlanner/src/org/jimple/planner/exceptions/InvalidTaskException.java
+++ JimplePlanner/src/org/jimple/planner/exceptions/InvalidTaskException.java
@@ -1,11 +1,13 @@
-package org.jimple.planner.storage;
+package org.jimple.planner.exceptions;
 
 public class InvalidTaskException extends Exception{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4657571341921237215L;
-
+	
+	public InvalidTaskException(){}
+	
 	public InvalidTaskException(String message){
 		super(message);
 	}
