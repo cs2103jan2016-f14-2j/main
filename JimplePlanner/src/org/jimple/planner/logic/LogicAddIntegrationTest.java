a69f72b13f843dfe578074fa3e008a1ed6de79fa
bsweeyee <Brandon@47-193.priv23.nus.edu.sg>
2016-04-01 10:24:09 +0800

    Logic: added add integration for parser, logic and storage

--- JimplePlanner/src/org/jimple/planner/logic/LogicAddIntegrationTest.java
+++ JimplePlanner/src/org/jimple/planner/logic/LogicAddIntegrationTest.java
@@ -7,11 +7,13 @@ import java.time.LocalDateTime;
 import org.jimple.planner.Constants;
 import org.junit.Test;
 
-public class LogicIntegrationTest {
+public class LogicAddIntegrationTest {
 	Logic testLogic = new Logic();
 
 	@Test
 	public void ShouldTestFromAndToTime() throws IOException	{
+		testLogic.getEventsList().clear();
+		
 		testLogic.execute("ADD task 1 FROM 1pm TO 3pm");
 		testLogic.execute("ADD task 2 FROM 12 mar 1300 TO 12 mar 1400");
 		testLogic.execute("ADD task 3 FROM today TO tomorrow");
@@ -23,7 +25,7 @@ public class LogicIntegrationTest {
 		assertEquals("2016-03-12T14:00", testLogic.getEventsList().get(0).getToTimeString());
 		
 		assertEquals("task 3", testLogic.getEventsList().get(1).getTitle());
-		assertEquals(LocalDateTime.now(), testLogic.getEventsList().get(1).getFromTimeString());
+		assertEquals(String.valueOf(LocalDateTime.now()).substring(0, 16), testLogic.getEventsList().get(1).getFromTimeString());
 		assertEquals(LocalDateTime.now().toLocalDate().plusDays(1) + "T23:59", testLogic.getEventsList().get(1).getToTimeString());
 		
 		assertEquals("task 5", testLogic.getEventsList().get(2).getTitle());
@@ -39,14 +41,53 @@ public class LogicIntegrationTest {
 		assertEquals(LocalDateTime.now().toLocalDate().plusDays(1) + "T19:00", testLogic.getEventsList().get(4).getToTimeString());
 	}
 	
-	/*@Test
+	@Test
 	public void ShouldTestByTime() throws IOException	{
-		testLogic.execute("ADD task 6 ON 10 dec");
-		testLogic.execute("ADD task 7 ON 11 dec 3pm");
+		testLogic.getDeadlinesList().clear();
+		
 		testLogic.execute("ADD task 9 BY monday");
 		testLogic.execute("ADD task 10 BY today");
 		testLogic.execute("ADD task 11 BY tomorrow");
+		
+		assertEquals("task 10", testLogic.getDeadlinesList().get(0).getTitle());
+		assertEquals(String.valueOf(LocalDateTime.now()).substring(0, 16), testLogic.getDeadlinesList().get(0).getFromTimeString());
+		
+		assertEquals("task 11", testLogic.getDeadlinesList().get(1).getTitle());
+		assertEquals(LocalDateTime.now().plusDays(1).toLocalDate() + "T23:59", testLogic.getDeadlinesList().get(1).getFromTimeString());
+		
+		assertEquals("task 9", testLogic.getDeadlinesList().get(2).getTitle());
+		assertEquals("2016-04-04T23:59", testLogic.getDeadlinesList().get(2).getFromTimeString());
+	}
+	
+	@Test
+	public void ShouldTestOnTime() throws IOException	{
+		testLogic.getEventsList().clear();
+		
+		testLogic.execute("ADD task 6 ON 10 dec");
+		testLogic.execute("ADD task 7 ON 11 dec 3pm");
+		
+		assertEquals("task 6", testLogic.getEventsList().get(0).getTitle());
+		assertEquals("2016-12-10T00:00", testLogic.getEventsList().get(0).getFromTimeString());
+		assertEquals("2016-12-10T23:59", testLogic.getEventsList().get(0).getToTimeString());
+		
+		assertEquals("task 7", testLogic.getEventsList().get(1).getTitle());
+		assertEquals("2016-12-11T15:00", testLogic.getEventsList().get(1).getFromTimeString());
+		assertEquals("2016-12-11T23:59", testLogic.getEventsList().get(1).getToTimeString());
+	}
+	
+	@Test
+	public void ShouldTestAtTime() throws IOException	{
+		testLogic.getEventsList().clear();
+		
 		testLogic.execute("ADD task 12 AT 10pm");
 		testLogic.execute("ADD task 13 AT 13 dec 6pm");
-	}*/
+		
+		assertEquals("task 12", testLogic.getEventsList().get(0).getTitle());
+		assertEquals(LocalDateTime.now().toLocalDate() + "T22:00", testLogic.getEventsList().get(0).getFromTimeString());
+		assertEquals(LocalDateTime.now().toLocalDate() + "T23:00", testLogic.getEventsList().get(0).getToTimeString());
+	
+		assertEquals("task 13", testLogic.getEventsList().get(1).getTitle());
+		assertEquals("2016-12-13T18:00", testLogic.getEventsList().get(1).getFromTimeString());
+		assertEquals("2016-12-13T19:00", testLogic.getEventsList().get(1).getToTimeString());
+	}
 }
