8d4ecacde2814132d14f7e480aa5500d672d2953
Loh Victor <vic94loh@Hotmail.com>
2016-04-08 13:20:29 +0800

    Create a test package for all tests in JimplePlanner

---
+++ JimplePlanner/src/org/jimple/planner/test/AllTestSuites.java
@@ -0,0 +1,16 @@
+package org.jimple.planner.test;
+
+import org.jimple.planner.test.storage.StorageTestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+//@@author A0135808B
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	StorageTestSuite.class
+})
+
+public class AllTestSuites {
+
+}
