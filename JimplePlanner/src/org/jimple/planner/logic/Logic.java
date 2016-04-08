9a6b58aae690448f53df8495214e1044e3b1e597
bsweeyee <Brandon@47-193.priv23.nus.edu.sg>
2016-04-08 13:00:11 +0800

    Logic: changed string conflict to type conflict

--- JimplePlanner/src/org/jimple/planner/logic/Logic.java
+++ JimplePlanner/src/org/jimple/planner/logic/Logic.java
@@ -166,7 +166,7 @@ public class Logic implements LogicMasterListModification, LogicTaskModification
 			case Constants.STRING_CHECKCONFLICT:
 				conflictChecker.getConflictedTasks(parsedInput.getVariableArray(), deadlines, events, conflictedTasks);
 				feedback[0] = "";
-				feedback[1] = Constants.STRING_CHECKCONFLICT;
+				feedback[1] = Constants.TYPE_CONFLICTED;
 				break;
 			default:
 				feedback[0] = Constants.ERROR_WRONG_COMMAND_FEEDBACK;
