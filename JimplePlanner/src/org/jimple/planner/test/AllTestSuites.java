3f61417e6018d428ff6c83644145c5763ae45c3b
Loh Victor <vic94loh@Hotmail.com>
2016-04-08 13:42:00 +0800

    Moved task test case to test package

--- JimplePlanner/src/org/jimple/planner/test/AllTestSuites.java
+++ JimplePlanner/src/org/jimple/planner/test/AllTestSuites.java
@@ -1,6 +1,7 @@
 package org.jimple.planner.test;
 
 import org.jimple.planner.test.storage.StorageTestSuite;
+import org.jimple.planner.test.task.TaskTest;
 import org.jimple.planner.test.logic.LogicTestSuite;
 import org.jimple.planner.test.parser.ParserTest;
 import org.junit.runner.RunWith;
@@ -12,7 +13,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ 
 	StorageTestSuite.class,
 	ParserTest.class,
-	LogicTestSuite.class
+	LogicTestSuite.class,
+	TaskTest.class
 })
 
 public class AllTestSuites {
