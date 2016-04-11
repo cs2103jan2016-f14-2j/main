e62a259998e9a9be60816a34ac2cbe68ef4e1bae
Luke <leeluke_9@live.com>
2016-04-11 12:02:06 +0800

    Spacing

--- JimplePlanner/src/org/jimple/planner/JimplePlanner.java
+++ JimplePlanner/src/org/jimple/planner/JimplePlanner.java
@@ -3,8 +3,10 @@ package org.jimple.planner;
 import org.jimple.planner.ui.UiComponent;
 
 public class JimplePlanner {
+	
 	public static void main(String[] args){
 		UiComponent Jimple = new UiComponent();
 		Jimple.begin(args);
 	}
+	
 }
